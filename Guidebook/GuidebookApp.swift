f7d14d0dcc9fb2da85d0abc5ec835668fcdefda7
W Lawless <wlawless.sharpe@protonmail.com>
2020-10-21 17:37:02 -0400

    Initial Commit

---
+++ Guidebook/GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookApp.swift
+//  Guidebook
+//
+//  Created by W Lawless on 10/21/20.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
